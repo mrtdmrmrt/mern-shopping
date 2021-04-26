645dc2dc93dbd555118b22de3c8c124ce349c5ff
Mert Demir <m.demir@digitastic.app>
2021-04-26 15:28:15 +0300

    The cart item has been added.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,27 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Navbar = ({ click }) => {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
         <h2>MERN Shopping Cart</h2>
       </div>
+
       <ul className="navbar__links">
         <li>
           <Link to="/cart" className="cart__link">
             <i className="fas fa-shopping-cart"></i>
             <span>
-              Cart
-              <span className="cartlogo__badge">0</span>
+              Cart <span className="cartlogo__badge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
@@ -21,7 +29,7 @@ const Navbar = ({ click }) => {
           <Link to="/">Shop</Link>
         </li>
       </ul>
-      {/**Hamburger menu */}
+
       <div className="hamburger__menu" onClick={click}>
         <div></div>
         <div></div>
@@ -30,4 +38,5 @@ const Navbar = ({ click }) => {
     </nav>
   );
 };
+
 export default Navbar;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,15 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import {
-  composeWithDevTools,
-  composeWithTools,
-} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-//Reducers
+// Reducers
 import { cartReducer } from './reducers/cartReducers';
 import {
-  getProductDetailsReducer,
   getProductsReducer,
+  getProductDetailsReducer,
 } from './reducers/productReducers';
 
 const reducer = combineReducers({
@@ -17,10 +14,22 @@ const reducer = combineReducers({
   getProducts: getProductsReducer,
   getProductDetails: getProductDetailsReducer,
 });
+
 const middleware = [thunk];
+
+const cartItemsInLocalStorage = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : [];
+
+const INITIAL_STATE = {
+  cart: {
+    cartItems: cartItemsInLocalStorage,
+  },
+};
 
 const store = createStore(
   reducer,
+  INITIAL_STATE,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
